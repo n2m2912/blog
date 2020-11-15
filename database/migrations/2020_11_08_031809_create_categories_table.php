214996991c4399ed0488754f5f2bb4f510270e07
n2m2912 <mannguyen281129@gmail.com>
2020-11-15 17:17:49 +0700

    update database

---
+++ database/migrations/2020_11_08_031809_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('father_id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+            $table->foreign('father_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
